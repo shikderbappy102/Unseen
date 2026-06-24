cad46447df90bf4a80bfaecfde54a51e05f5457c
shikderbappy102 <shikderbappy102@gmail.com>
2026-06-25 01:32:42 +0600

    Update main.tsx

--- artifacts/unseen/src/main.tsx
+++ artifacts/unseen/src/main.tsx
@@ -1,18 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import "./index.css";
 
 document.documentElement.classList.add("dark");
 
-const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!clerkPublishableKey) {
-  throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY");
-}
-
-createRoot(document.getElementById("root")!).render(
-  <ClerkProvider publishableKey={clerkPublishableKey}>
-    <App />
-  </ClerkProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
